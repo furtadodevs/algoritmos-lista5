86ca303461f75d52d6fb75d91b98773242dc95a4
Gabrielle Furtado <gabriellerodriguesfurtado@gmail.com>
2026-07-17 21:49:39 -0300

    Exemplo

---
+++ a_exemplo_switch.php
@@ -0,0 +1,44 @@
+<?php
+$dia = 7;
+$valor = 100.00;
+
+switch ($dia) {
+    case 1:
+        echo ("Domingo");
+        break;
+    case 2:
+        echo ("Segunda");
+        break;
+    case 3:
+        echo ("Teça");
+        break;
+    case 4:
+        echo ("Quarta");
+        break;
+    case 5:
+        echo ("Quinta");
+        break;
+    case 6:
+        echo ("Sexta");
+        break;
+        case 7:
+            echo ("Sexta");
+            break;
+            default:
+            echo ("Opção inválida");
+            break;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
